90d62ebca096c3891ef315bd9792eed1db29cf8a
tonmoy00saha <tonmoy.official.20@gmail.com>
2024-11-26 12:12:45 +0600

    cpp file added to github

---
+++ my.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        cout<<"Push to github"<<endl;
+    }
+}
